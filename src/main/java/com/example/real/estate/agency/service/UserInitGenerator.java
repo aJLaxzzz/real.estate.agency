db04f32deb6789834f66aedbdc5d65a42ddb3973
Admin <aj1ax@yandex.ru>
2024-05-10 17:54:57 +0300

    + last commit

--- src/main/java/com/example/real/estate/agency/service/UserInitGenerator.java
+++ src/main/java/com/example/real/estate/agency/service/UserInitGenerator.java
@@ -2,6 +2,7 @@ package com.example.real.estate.agency.service;
 
 import com.example.real.estate.agency.entity.User;
 import com.example.real.estate.agency.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,12 @@ import java.util.List;
 @Service
 public class UserInitGenerator {
     @Autowired
-    private static UserRepository userRepository;
+    private UserRepository userRepository;
     @Autowired
-    private static PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
-    public static void main() {
+    @PostConstruct
+    public void generateUsers() {
         List<User> users = List.of(
             new User(
                 null,
